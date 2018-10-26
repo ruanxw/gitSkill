bf5e5f9a601760ee5d0490be89bfa9fc5decfa3f
ruanxw <411235314@qq.com>
2018-10-26 20:35:07 +0800

    alien_invision 创建窗口类

---
+++ pyCode/alien/ai_settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """存储所有设置的类"""
+            
+    def __init__(self):
+        """初始化设置"""
+        #屏幕设置
+        self.screen_width = 500
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
